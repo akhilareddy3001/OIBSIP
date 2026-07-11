aa35657692d4d871c29d55c7fd5afaad46bb459c
Akhila Reddy <akhilareddy3258@gmail.com>
2026-07-11 22:44:10 +0530

    Connected MongoDB Atlas successfully

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors"); 
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+dotenv.config();
+
+connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
